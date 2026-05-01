250666af5bb63c813bd0435356c3c896d3c5ba7d
Pablo Toapanta <toapantapablo233@gmail.com>
2026-04-30 22:34:57 -0500

    Elaboracion modelo viajes

--- URide/viajes/models.py
+++ URide/viajes/models.py
@@ -13,3 +13,18 @@ class Vehiculo(models.Model):
     color=models.CharField( max_length=50)
     placa=models.CharField( max_length=50, unique=True)
     max_capacidad=models.PositiveSmallIntegerField(validators=[MinValueValidator(2),MaxValueValidator(6)])
+
+class Viaje(models.Model):
+    class EstadoViaje(models.TextChoices):
+        NO_INICIADO="NO_INICIADO",'No iniciado'
+        EN_CURSO='EN_CURSO','En curso'
+        FINALIZADO='FINALIZADO','Finalizado'
+        CANCELADO='FINALIZADO','Cancelado'
+    estado_viaje=models.CharField(max_length=20,choices=EstadoViaje.choices,default=EstadoViaje.NO_INICIADO)
+    auto=models.ForeignKey(Vehiculo,on_delete=models.CASCADE)
+    zona_origen=models.CharField( max_length=50)
+    zona_destino=models.CharField( max_length=50)
+    fecha_hora_salida=models.DateTimeField(verbose_name="Fecha y hora de salida",help_text="Formato: DD/MM/YYYY HH:MM")
+    asientos_disponibles=models.PositiveSmallIntegerField(validators=[MinValueValidator(2),MaxValueValidator(6)])
+    def __str__(self):
+        return f"Viaje {self.id} - {self.zona_origen} -> {self.zona_destino} - {self.fecha_hora_salida}"
